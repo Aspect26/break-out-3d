faf2f6e95ea90f852fe1a8dd4df46be2d533044f
Aspect26 <flimmelj@gmail.com>
2018-01-04 14:58:11 +0100

    Initial commit

---
+++ Assets/BallBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour {
+
+    public Vector3 speed;
+    public Vector3 direction;
+    public Rigidbody rb;
+    public Vector3 movement;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        speed = new Vector3(0.5f, 0.5f, 0.5f);
+        direction = new Vector3(-1f, 0.3f, 0.2f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        movement = new Vector3(speed.x * direction.x, speed.y * direction.y, speed.z * direction.z);
+    }
+
+    void FixedUpdate()
+    {
+        this.rb.velocity = movement;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        //Check if you have to be specific to an object to bounce
+        Debug.Log("called");
+
+        Vector3 bouncedVector = -2 * (Vector3.Dot(this.direction, other.contacts[0].normal)) * other.contacts[0].normal + this.direction;
+        this.direction = bouncedVector;
+
+    }
+
+}
---
+++ Assets/PaddleBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PaddleBehavior : MonoBehaviour {
+
+    private const float CUBE_WIDTH = 0.744f * 2f + 0.125f;
+    private const float CUBE_HEIGHT = 1.0f - 0.0625f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float mouseX = Input.mousePosition.x;
+        float newZ = mouseX / (Screen.width / CUBE_WIDTH) - (CUBE_WIDTH / 2);
+
+        float mouseY = Input.mousePosition.y;
+        float newY = mouseY / (Screen.height / CUBE_HEIGHT);
+
+        this.transform.SetPositionAndRotation(new Vector3(this.transform.position.x, newY, newZ), this.transform.rotation);
+	}
+}
