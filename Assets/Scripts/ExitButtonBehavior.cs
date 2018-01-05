7162293a430185a5a72de857fc0d190fdf03a370
Aspect26 <flimmelj@gmail.com>
2018-01-05 13:26:53 +0100

    Added main menu
    Closes #6

---
+++ Assets/Scripts/ExitButtonBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ExitButtonBehavior : MonoBehaviour {
+
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(TaskOnClick);
+        this.GetComponentInChildren<Text>().text = "Exit";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/PlayButtonBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButtonBehavior : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(TaskOnClick);
+        this.GetComponentInChildren<Text>().text = "Play";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+}
